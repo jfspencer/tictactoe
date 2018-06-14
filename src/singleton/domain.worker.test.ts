79de1393b4ac09e760bea8ef7bbe64b4ded0e388
James Spencer <thisjustosay@gmail.com>
2018-06-14 13:20:13 -0600

    general case coverage on game mechanics

--- src/singleton/domain.worker.test.ts
+++ src/singleton/domain.worker.test.ts
@@ -24,7 +24,6 @@ describe('Service: DomainWorker', () => {
     ]
   };
   it('determineWinner should return player0 for across the top data', () => {
-    debugger;
     const winner = domainWorker.determineWinner(
       player0AcrossTop,
       [
@@ -34,31 +33,37 @@ describe('Service: DomainWorker', () => {
     expect(winner.name).toBe('Christopher Nolan');
   });
 
-  // const player1AcrossMiddle: MoveSequence = {
-  //   _id: '123', gameId: 'string', type: RecordType.MoveSequence,
-  //   moves: [
-  //     {move: 'UL', turn: 0, time: 1}, {move: 'LM', turn: 1, time: 2}, {move: 'UM', turn: 0, time: 3},
-  //     {move: 'LL', turn: 1, time: 4}, {move: 'UR', turn: 0, time: 5}, {move: 'LR', turn: 1, time: 3}
-  //   ]
-  // };
-  // it('determineWinner should return player1 for across the middle data', () => {
-  //   const res = domainWorker.determineWinner(player1AcrossMiddle, []);
-  //   expect(true).toBe(true);
-  // });
 
+  const gameInProgress: MoveSequence = {
+    _id: '123', gameId: 'string', type: RecordType.MoveSequence,
+    moves: [
+      {move: 'UL', turn: 0, time: 1}, {move: 'LM', turn: 1, time: 2}, {move: 'UM', turn: 0, time: 3}
+    ]
+  };
+  it('determineWinner should return null for gameInProgress data', () => {
+    const winner = domainWorker.determineWinner(
+      gameInProgress,
+      [
+        {name: 'Christopher Nolan', turnId: 0, dbId: 'player0'},
+        {name: 'Alfred Hitchcock', turnId: 1, dbId: 'player1'}
+      ]);
+    expect(winner).toBe(null);
+  });
+
+  const player1AcrossBottom: MoveSequence = {
+    _id: '123', gameId: 'string', type: RecordType.MoveSequence,
+    moves: [
+      {move: 'MM', turn: 0, time: 1}, {move: 'LL', turn: 1, time: 2}, {move: 'UM', turn: 0, time: 3},
+      {move: 'LM', turn: 1, time: 4}, {move: 'UR', turn: 0, time: 5}, {move: 'LR', turn: 1, time: 3}
+    ]
+  };
+  it('determineWinner should return player0 for across the top data', () => {
+    const winner = domainWorker.determineWinner(
+      player1AcrossBottom,
+      [
+        {name: 'Christopher Nolan', turnId: 0, dbId: 'player0'},
+        {name: 'Alfred Hitchcock', turnId: 1, dbId: 'player1'}
+      ]);
+    expect(winner.name).toBe('Alfred Hitchcock');
+  });
 });
-
-
-
-
-
-const player0AcrossBottom = [];
-
-const player1DownLeft = [];
-const player0DownMiddle = [];
-const player1DownRight = [];
-
-const player0DiagonalRight = [];
-const player1DiagonalLeft = [];
-const gameInProgress = [];
-const toManyMoves = [];
